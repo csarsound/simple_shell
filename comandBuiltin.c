0bb382b6ebbed95b67cad56c929556f13ff76211
csarsound <68489879+csarsound@users.noreply.github.com>
2022-04-21 13:34:53 -0500

    Shell

--- comandBuiltin.c
+++ comandBuiltin.c
@@ -9,3 +9,17 @@ int cd(char *path)
 {
 	return (chdir(path));
 }
+
+char *parsing_cmd(char **cmd)
+{
+    
+	if (!strcmp(cmd[0], "exit"))
+	{
+		clear_memory(cmd);
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		return (cmd[0]);
+	}
+}
--- shell.h
+++ shell.h
@@ -10,15 +10,12 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <stdbool.h>
-extern char **environ;
 
 int cd(char *path);
 char **get_input(char *input);
 char *readLine(void);
 void signal_handler(int sig_num);
 char *parsing_cmd(char **cmd);
-int _strlen(char *s);
 void clear_memory(char **args);
-int _strcmp(char *s1, char *s2);
 
 #endif
